d67d1c100282e31f40014653c0a515db6b0b248d
AlejandroEN <alejandroescobarnunez@gmail.com>
2025-05-08 03:02:07 -0500

    cleanup: remove ignored directories from repo

--- dist/dto/create-post.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreatePostDto {
-    title: string;
-    description: string;
-    tags: string[];
-    price: number;
-    presentation_card_id: string;
-    images: string[];
-    seller_id: string;
-}
